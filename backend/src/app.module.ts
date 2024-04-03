8fcfc0f50d0f25d826b85d622aa5a898b6389eba
gabrielsaviank <gabrielsvnzrd@gmail.com>
2024-04-03 14:00:54 -0300

    feat: generated initial controllers, repo etc...

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AddressesModule } from './addresses/addresses.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AddressesModule],
   controllers: [AppController],
   providers: [AppService],
 })
