b0300a242540968ec8aca006825abfe9e1b79a74
Gabriel Savian <gabrielsvnzrd@gmail.com>
2024-04-03 15:16:14 -0300

    feat: created persons generated files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,23 @@
 import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AddressesModule } from './addresses/addresses.module';
+import { UserEntity } from './users/users.entity';
+import { PersonsModule } from './persons/persons.module';
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-        type: "sqlite",
-        database: 'db.sqlite',
-        entities: [],
-        synchronize: true
-      }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
     UsersModule,
-    AddressesModule
+    AddressesModule,
+    PersonsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
