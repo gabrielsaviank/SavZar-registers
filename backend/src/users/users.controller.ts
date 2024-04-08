32751b449d3ddf37b11deb291f772bc3537bf923
Gabriel Savian <gabrielsvnzrd@gmail.com>
2024-04-08 14:10:41 -0300

    feat: finished cookie

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -17,21 +17,38 @@ import { UserEntity } from './users.entity';
 import { UpdateUserDto } from './dtos/update-user-dto';
 import { CurrentUser } from './decorators/current-user.decorator';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
 
+  @Get('/whoami')
+  whoAmI(@Session() session: Record<string, any>): Promise<UserEntity> {
+    return this.usersService.findOneById(session.userId);
+  }
+
   @Post('/signup')
-  async createUser(@Body() body: CreateUserDto): Promise<UserEntity> {
-    return this.authService.signUp(body.email, body.password);
+  async createUser(
+    @Body() body: CreateUserDto,
+    @Session() session: any,
+  ): Promise<UserEntity> {
+    const user = await this.authService.signUp(body.email, body.password);
+    session.userId = user.id;
+
+    return user;
   }
 
   @Post('/signin')
-  async signIn(@Body() body: CreateUserDto): Promise<UserEntity> {
-    return this.authService.signIn(body.email, body.password);
+  async signIn(
+    @Body() body: CreateUserDto,
+    @Session() session: any,
+  ): Promise<UserEntity> {
+    const user = await this.authService.signIn(body.email, body.password);
+    session.userId = user.id;
+
+    return user;
   }
 
   @Get('/:id')
@@ -63,18 +80,13 @@ export class UsersController {
     return this.usersService.update(id, body);
   }
 
-  // @Get('/whoami')
-  // whoAmI(@Session() session: Record<string, any>): Promise<UserEntity> {
-  //   return this.usersService.findOneById(session.userId);
-  // }
-
   @Post('/logout')
   signOut(@Session() session: Record<string, any>) {
     session.userId = null;
   }
 
-  @Get('/whoami')
-  whoAmI(@CurrentUser() user: UserEntity) {
-    return user;
-  }
+  // @Get('/whoami')
+  // whoAmI(@CurrentUser() user: UserEntity) {
+  //   return user;
+  // }
 }
