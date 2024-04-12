d3e63ba6cc5ab7e81227cb93ff364f0deac9aefe
Gabriel Savian <gabrielsvnzrd@gmail.com>
2024-04-12 11:42:29 -0300

    fix: fixed privateroute validation

--- frontend/src/helpers/PrivateRoute.js
+++ frontend/src/helpers/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PrivateRoute = () => {
-    const isAuthenticated = useSelector(state => state?.auth);
+    const { isAuthenticated } = useSelector(state => state?.auth);
 
     return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
